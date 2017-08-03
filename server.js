b2991c7ef1129924433ba4cec3792676457a2f18
ganesh891 <ganeshwizard777@gmail.com>
2017-08-03 22:40:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,19 +10,19 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article one', function(req,res){
-    res.send("Article one request is here and served here")}
+    res.send('Article one request is here and served here')}
 );
 
-app.get('/Article Two', function(req,res){
-    res.send("Article Two request is here and served here")}
+app.get('/article two', function(req,res){
+    res.send('Article Two request is here and served here')}
 );
 
-app.get('/article Three', function(req,res){
-    res.send("Article Three request is here and served here")}
+app.get('/article three', function(req,res){
+    res.send('Article Three request is here and served here')}
 );
 
 app.get('/article Four', function(req,res){
-    res.send("Article Four request is here and served here")}
+    res.send('Article Four request is here and served here')}
 );
 
 app.get('/ui/style.css', function (req, res) {
