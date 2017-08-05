a616dea6855cc7ae7d427677b1568fb873eb5137
ganesh891 <ganeshwizard777@gmail.com>
2017-08-05 13:18:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,8 +62,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req,res){
-   res.send(createTemplate('articleone'));
+app.get('/articleone', function(req,res){
+   res.send(createTemplate(articleone));
 });
 
 app.get('/article-two', function(req,res){
