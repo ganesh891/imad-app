913fa15051832e81b25747fbe82d73a6f804c70e
ganesh891 <ganeshwizard777@gmail.com>
2017-08-08 00:15:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,11 @@
-var button=document.getElementById('counter');
-    
-    button.onclick = function()
-    {
-        
-        var request = new XMLHttpRequest();
-        
-        request.onreadystatechange = function()
-        {
-            if(request.readystate===XMLHttpRequest.DONE){
-                if (request.status===200)
-                {
-                    var counter = request.responseText;
-                    var span = document.getElementById('count');
-                    span.innerHTML = counter.toString();
-                }
-            }
-        };
-        request.open('GET','http://ganeshwizard777.imad.hasura-app.io/counter',true);
-        request.send(null);
-    };
+console.log('loaded!');
+
+var element = document.getElementById('main-text');
+element.innerHTML='New Value';
+
+
+var img= document.getElementById('madi');
+img.onclick =function()
+{
+    img.style.marginleft='100px';
+};
