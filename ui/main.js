0cc4bc888a21841e883520c34b3f80a9a88e4c2d
ganesh891 <ganeshwizard777@gmail.com>
2017-08-07 10:52:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,29 +1,22 @@
-console.log('Loaded!');
 
-
-var element = document.getElementById("main-text");
-
-element.innerHTML='New value';
-
-//move the image
-
-/*var img= document.getElementById(`madi`);
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+5;
-    img.style.marginLeft = marginLeft+px;
-    }
-    
-    img.onclick =function(){
-        var interval= setInterval(moveRight,50);
-    };
-    */
-    
     var button=document.getElementById("counter");
     var counter=0;
     button.onclick = function()
     {
-        counter=counter+1;
-        var span = document.getElementId('count');
-        span.innerHTML = counter.toString();
+        
+        var request = new XmlHttpRequest();
+        
+        request.onreadystatechange = fuction()
+        {
+            if(request.readystate===XmlHttpRequest.Done){
+                if (request.status===200)
+                {
+                    var counter = request.responseText;
+                    var span = document.getElementById('count');
+                    span.innerHtml = counter.toString();
+                }
+            }
+        }
+        request.open('Get','http://coco98.imad.hasura-app.io/couter',true);
+        request.send(null);
     };
