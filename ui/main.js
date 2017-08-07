33c1935629bdbeb51a2c56466a64ba7e53e6ddda
ganesh891 <ganeshwizard777@gmail.com>
2017-08-08 00:39:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,14 @@ element.innerHTML='New Value';
 
 
 var img= document.getElementById('madi');
+var marginLeft=0;
+function moveRight()
+{
+    marignLeft = marginLeft+10;
+    img.style.marginleft=marginleft+px;
+}
 img.onclick =function()
 {
+    var interval= setInterval(moveRight,100);
     img.style.marginLeft='100px';
 };
