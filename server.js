b5194861c0ad746e4d49a268b125ae996344564e
ganesh891 <ganeshwizard777@gmail.com>
2017-08-07 22:12:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,6 +62,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
 var counter=0;
 app.get('/counter', function(req,res)
 {
