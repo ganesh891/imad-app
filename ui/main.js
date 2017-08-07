a79659c9901c011ee4554cf555c3abf11cd32e1b
ganesh891 <ganeshwizard777@gmail.com>
2017-08-07 12:49:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,14 +5,14 @@ var button=document.getElementById("counter");
         
         var request = new XmlHttpRequest();
         
-        request.onreadystatechange = fuction()
+        request.onreadystatechange = function()
         {
-            if(request.readystate===XmlHttpRequest.Done){
+            if(request.readystate===XMLHttpRequest.DONE){
                 if (request.status===200)
                 {
                     var counter = request.responseText;
                     var span = document.getElementById('count');
-                    span.innerHtml = counter.toString();
+                    span.innerHTML = counter.toString();
                 }
             }
         }
