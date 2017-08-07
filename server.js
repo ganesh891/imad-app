c3f6bcf8d9ccf32949bf1e3f07c90c538925484f
ganesh891 <ganeshwizard777@gmail.com>
2017-08-07 22:26:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,10 +100,6 @@ app.get('/article-Four', function(req,res){
 
 
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
