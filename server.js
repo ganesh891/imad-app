0f33214ac5a1eddd2d54bfd6dd9d9dbc01ae3638
ganesh891 <ganeshwizard777@gmail.com>
2017-08-08 23:20:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var Pool = require('pg').Pool;
+
+var config ={
+    user :'',
+    database :'',
+    host :'',
+    port :'',
+    password :'process.env.DB_PASSWORD',
+    
+};
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -88,6 +99,21 @@ app.get('/article-three', function(req,res){
 app.get('/article-Four', function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-four.html')); 
     
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req,res){
+    pool.query('SELECT * FROM test', function(err,result)
+    {
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+           res.send(JSON.stringify(result)); 
+        }
+    });
 });
 
 app.get('/ui/main.js', function (req, res) {
