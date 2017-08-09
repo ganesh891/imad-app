349a98ac0178c2d0d566bbfccc4e11c38a8c5f11
ganesh891 <ganeshwizard777@gmail.com>
2017-08-09 17:50:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -158,9 +158,9 @@ app.get('/test-db', function(req,res){
 
 
 var names=[];
-app.get('/submit-name/:name', function (req, res)
+app.get('/submit-name', function (req, res)
 { 
-    var name= req.params.name;
+    var name= req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
     
