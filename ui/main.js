902495507b23c2e54b427293d3d44ba0d8b3c1f3
ganesh891 <ganeshwizard777@gmail.com>
2017-08-09 18:25:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -79,7 +79,7 @@ submit.onclick = function()
     
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
-    request.open('GET','http://ganeshwizard777.imad.hasura-app.io/submit-name?name=' +name, true);
+    request.open('GET','http://ganeshwizard777.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
     
  };
