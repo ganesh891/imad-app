8de90f1b2889aaa33045c8917047906ea14e74ef
ganesh891 <ganeshwizard777@gmail.com>
2017-08-09 15:27:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,15 @@ img.onclick = function(){
     var interval= setInterval(moveRight, 50);
     
 };
+
+
+//counter
+
+var button = document.getElementById('counter');
+var counter =0;
+button.onclick =function()
+{
+    counter= counter+1;
+var span = document.getElementId('count');
+span.innerHTML = counter.toString();
+};
