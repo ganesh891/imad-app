2bb0cc105ef400c3d96b273b97ddd02f2c37ef19
ganesh891 <ganeshwizard777@gmail.com>
2017-08-10 11:04:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,6 +123,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var names=[];
+app.get('/submit-name', function (req, res)
+{ 
+    var name= req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+    
+});
 
 
 var counter=0;
@@ -132,14 +140,6 @@ app.get('/counter', function(req,res)
    res.send(counter.toString());
 });
 
-var names=[];
-app.get('/submit-name', function (req, res)
-{ 
-    var name= req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-    
-});
 
 
      app.get('/:articleName', function(req,res){
