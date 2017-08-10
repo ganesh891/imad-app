3b202826cc2f6a18080a6e1798d879b724fd3011
ganesh891 <ganeshwizard777@gmail.com>
2017-08-10 21:54:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ app.get('/submit-name', function (req, res)
 
 
 var pool = new Pool(config);
-app.get('/test-db', function(req,res){
+app.get('/testdb', function(req,res){
     pool.query('SELECT * FROM test-db', function(err,result)
     {
         if(err)
