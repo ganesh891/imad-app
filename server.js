5baad31361c827c357755740eb0aae82d30604ff
ganesh891 <ganeshwizard777@gmail.com>
2017-08-18 00:26:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -229,7 +229,7 @@ app.get('/articles/:articleName', function(req,res)
        //articleName =article-one
        //articles[articlesname]={}
        
-       pool.query("SELECT * FROM articles WHERE title = $1", [req.params.articleName], function(err,result)
+       pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err,result)
       {
            if(err)
            {
